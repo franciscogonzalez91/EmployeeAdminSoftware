74551ca76f98fdf28d7f13ce3eb3d383b3b0fcb8
Jose Francisco Gonzalez Bustamante <francisco.gonzalez.bustamante@gmail.com>
2015-08-15 12:34:56 -0700

    15/08/2015 12:24 PM
    Add some comments

--- Employee Admin DB Connection/DataBaseConnection.cs
+++ Employee Admin DB Connection/DataBaseConnection.cs
@@ -7,32 +7,40 @@ namespace Employee_Admin_DB_Connection
 {
     public class DataBaseSqlConnection
     {
+        //Objects
         /// <summary>
-        /// 
+        /// Object to create the connection to the database
         /// </summary>
         private SqlConnection SqlConn = new SqlConnection();
         /// <summary>
-        /// 
+        /// This object run the SQL Sentence
         /// </summary>
         private SqlCommand Sqlcmd = new SqlCommand();
         /// <summary>
-        /// 
+        /// This object read the SQL Data
         /// </summary>
         private SqlDataReader SqlReader;
         /// <summary>
-        /// 
+        /// This object sabe the information of the table of the database
         /// </summary>
         private DataTable Table = new DataTable("Empleados");
+        
+        //Variables
         /// <summary>
-        /// 
+        /// This variable have the connection string information
         /// </summary>        
         private string Connection = "Data Source=FRANK-PC;Initial Catalog=EmployeeAdmon;Integrated Security=True";
         /// <summary>
         /// 
         /// </summary>
-        private string SqlSentence;
+        //private string SqlSentence;
         
-        
+        //Functions
+        /// <summary>
+        /// This Function return the information of the information searched on the database
+        /// </summary>
+        /// <param name="DataSearched"></param>
+        /// <returns>Table</returns>
         public DataTable SearchEmployee(string DataSearched)
         {
             //Connection to the database
